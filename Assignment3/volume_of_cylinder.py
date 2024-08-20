c6bf09a3774651a610dfe3af97facf03caa17495
mkhurram2 <m.khurram2@outlook.com>
2024-08-20 13:42:56 +0000

    Update

---
+++ Assignment3/volume_of_cylinder.py
@@ -0,0 +1,6 @@
+print("****calculates the volume of a cylinder****")
+r = int(input("enter the Radius = "))
+h = int(input("enter the Height = "))
+pi=3.14
+v = pi * (r**2) * h
+print ("Volume of a cylinder is = ",v )
